ddb762869c7c2cb5924cc9ef47e54d46a0f0ade9
Vittorio Sassu <100283240+vickysX@users.noreply.github.com>
2023-08-25 20:28:05 +0200

    create airport item

--- app/src/main/java/com/example/flightsearch/ui/FlightSearchApp.kt
+++ app/src/main/java/com/example/flightsearch/ui/FlightSearchApp.kt
@@ -1,13 +1,17 @@
 package com.example.flightsearch.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocalAirport
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
@@ -22,6 +26,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -36,8 +44,13 @@ fun FlightSearchApp(
     viewModel: FlightSearchViewModel = hiltViewModel()
 ) {
     val userQuery = viewModel.queryString.collectAsState()
+    val suggestions = viewModel.suggestions.collectAsState()
     val flights = viewModel.flights
     val favoriteFlights = viewModel.favoriteFlights
+    val addToFav : (Flight) -> Unit = {viewModel.addToFavorites(it)}
+    val removeFromFav : (Flight) -> Unit = {
+        viewModel.removeFromFavorites(it)
+    }
     Scaffold(
         modifier = modifier,
         topBar = {
@@ -53,7 +66,12 @@ fun FlightSearchApp(
         AppBody(
             modifier = Modifier.padding(paddingValues),
             query = userQuery.value,
-            updateQuery = {viewModel.updateQueryString(it)}
+            updateQuery = {viewModel.updateQueryString(it)},
+            suggestions = suggestions.value,
+            flights = flights,
+            favoriteFlights = favoriteFlights,
+            addToFav = addToFav,
+            removeFromFav = removeFromFav
         )
     }
 }
@@ -62,7 +80,12 @@ fun FlightSearchApp(
 fun AppBody(
     modifier: Modifier = Modifier,
     query: String,
-    updateQuery: (String) -> Unit
+    updateQuery: (String) -> Unit,
+    suggestions: List<Airport>,
+    flights: List<Flight>,
+    favoriteFlights: List<Flight>,
+    addToFav: (Flight) -> Unit,
+    removeFromFav: (Flight) -> Unit
 ) {
     Column(
         modifier = modifier
@@ -72,6 +95,15 @@ fun AppBody(
             onValueChange = updateQuery
         )
         /** TODO: Conditional rendering of lists **/
+        if (query.isEmpty()) {
+            Flights(
+                flights = favoriteFlights,
+                addToFav = addToFav,
+                removeFromFav = removeFromFav
+            )
+        } else {
+
+        }
     }
 }
 
@@ -118,10 +150,33 @@ fun Suggestions(
 
 @Composable
 fun AirportItem(
-    modifier: Modifier
+    modifier: Modifier = Modifier,
+    airport: Airport
 ) {
-    Row {
-        // TODO: Icon and text with span
+    Row (
+        modifier = modifier.clickable {
+            // TODO: Define function to make appear the list of flights
+        }
+    ) {
+        val airportName = buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Bold
+                )
+            ) {
+                append(airport.iataCode)
+            }
+            append(" ")
+            append(airport.name)
+        }
+        Icon(
+            imageVector = Icons.Default.LocalAirport,
+            contentDescription = stringResource(
+                id = R.string.airport_item
+            )
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = airportName)
     }
 }
 
@@ -179,5 +234,20 @@ fun FlightCard(
 fun SearchBarPreview() {
     FlightSearchTheme {
         SearchBar(value = "", onValueChange = {})
+    }
+}
+
+@Composable
+@Preview
+fun AirportItemPreview() {
+    FlightSearchTheme {
+        AirportItem(
+            airport = Airport(
+                id = 0,
+                iataCode = "FCO",
+                name = "Leonardo Da Vinci International Airport",
+                passengers = 1_000_000
+            )
+        )
     }
 }
